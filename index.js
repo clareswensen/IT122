369c484637b654c5a0f71964067f57b3beb16e75
clareswensen <clare.swensen@seattlecolleges.edu>
2022-04-13 18:08:59 -0700

    homework 2 second commit

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ import {getAll, getItem} from './data.js';
 import http from 'http';
 import fs from 'fs';
 
-
-
 http.createServer(function(req,res) {
   console.log(req.url)
   var path = req.url.toLowerCase();
   switch(path) {
     case '/':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(JSON.stringify(getAll()));
+      res.end(JSON.stringify(getAll());
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About me: Hello, My name is Clare.');
+      res.end('About me: Hello, My name is Clare. I love oysters');
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
