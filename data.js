04ae8f93746d278b68297dd3f628da0b5abfe3e2
clareswensen <clareswensen.seattlecolleges.com>
2022-04-18 19:31:01 -0700

    functions, modules

--- data.js
+++ data.js
@@ -1,18 +1,42 @@
 const oysters = [
-    {name : "Olympia", scientificName : "Ostrea lurida", origin : "Pacific Coast", flavor : "mushrooms and celery salt"},
-    {name: "Kumamoto", scientificName: "Crassostrea sikamea", origin: "Southwest Japan", flavor: "sweet"},
-    {name: "Pacific", scientificName: "Crassostrea gigas", origin: "Japan Pacific Coast", flavor: "cucumber and melon"},
-    {name: "European Flat", scientificName: "Ostrea edulis", origin: "Europe", flavor: "smoke and metal"},
-    {name: "Sydney Rock", scientificName: "Saccostrea glomerata", origin: "Australia", flavor: "sweet"}
+    { name : "Olympia", 
+    scientificName : "Ostrea lurida", 
+    origin : "Pacific Coast", 
+    flavor : "mushrooms and celery salt"},
+
+    { name : "Kumamoto", 
+    scientificName : "Crassostrea sikamea", 
+    origin: "Southwest Japan", 
+    flavor: "sweet"},
+
+    { name : "Pacific", 
+    scientificName : "Crassostrea gigas", 
+    origin : "Japan Pacific Coast", 
+    flavor : "cucumber and melon"},
+
+    { name : "European Flat", 
+    scientificName : "Ostrea edulis", 
+    origin : "Europe", 
+    flavor: "smoke and metal"},
+    
+    { name : "Sydney Rock", 
+    scientificName : "Saccostrea glomerata", 
+    origin : "Australia", 
+    flavor: "sweet"}
 ];
 
 // getAll method, returns all array items
 export const getAll = (item) => {
-    return(oysters);
-};
-//getItem method, returns a single item
-export const getItem = (item) => {
-    return(oysters[1]);
-}
+    return oysters;
+};  
+console.log(getAll);
 
-// Export getAll() and getItem()
+//getItem method, returns a single item
+export const getItem = (val) => {
+    const findOyster = (oyster) => {
+        return oyster.name.toLowerCase() === val.toLowerCase();
+    }
+        const result = oysters.find(findOyster);
+        return result;
+    }
+
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import {getAll, getItem} from './data.js';
 import http from 'http';
-import fs from 'fs';
+import { parse } from "querystring";
+import * as oyster from "./data.js";
+
 
 http.createServer(function(req,res) {
-  console.log(req.url)
+  console.log('createServer got request')
   var path = req.url.toLowerCase();
-  switch(path) {
+  let url_parts = req.url.split("?");
+  let query = parse(url_parts[1]);
+  
+  switch(url_parts[0]) {
     case '/':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(JSON.stringify(getAll());
+      res.end(JSON.stringify(oyster.getAll(), null, '\t'));
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About me: Hello, My name is Clare. I love oysters');
+      break;
+    case '/detail':
+      let found = oyster.getItem(query.name); // get oyster object
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results = (found) ? JSON.stringify(found,null, '\t') : "Not found";
+      res.end('Results for ' + query.name + "\n" + results);
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
